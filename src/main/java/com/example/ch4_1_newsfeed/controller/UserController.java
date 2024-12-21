605653220cad5d92518e734760ba8e6d98d18f65
박병천 <genius178896@gmail.com>
2024-12-21 11:59:21 +0900

    WIP : USER 기능

--- src/main/java/com/example/ch4_1_newsfeed/controller/UserController.java
+++ src/main/java/com/example/ch4_1_newsfeed/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.example.ch4_1_newsfeed.controller;
 
+
+import com.example.ch4_1_newsfeed.request.LoginRequest;
+import com.example.ch4_1_newsfeed.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
+
+        Long userId = userService.getUserId(loginRequest);
+
+        session.setAttribute("userId", userId);
+
+        return new ResponseEntity<>(userId, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/example/ch4_1_newsfeed/service/UserService.java
+++ src/main/java/com/example/ch4_1_newsfeed/service/UserService.java
@@ -1,4 +1,17 @@
 package com.example.ch4_1_newsfeed.service;
 
+import com.example.ch4_1_newsfeed.repository.UserRepository;
+import com.example.ch4_1_newsfeed.request.LoginRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long getUserId(LoginRequest request) {
+        userRepository.findByEmail(request.getEmail());
+    }
 }
