df1a5572fd363fd5b8d40a70d5e0d4b1eaee525a
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-24 22:57:44 +0900

    🩹: 애노테이션 삭제

--- src/main/java/com/example/ch4_1_newsfeed/config/WebConfig.java
+++ src/main/java/com/example/ch4_1_newsfeed/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableJpaAuditing
 public class WebConfig {
 
     @Bean
