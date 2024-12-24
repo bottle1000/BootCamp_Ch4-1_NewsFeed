67dc6f37c928627ae5c599d2b1b6f1c9611a7699
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-24 22:57:12 +0900

    🩹: 임포트 삭제, 애노테이션 붙임

--- src/main/java/com/example/ch4_1_newsfeed/dto/feed/FeedResponseDto.java
+++ src/main/java/com/example/ch4_1_newsfeed/dto/feed/FeedResponseDto.java
@@ -1,24 +1,18 @@
 package com.example.ch4_1_newsfeed.dto.feed;
 
 import com.example.ch4_1_newsfeed.entity.Feed;
+import com.example.ch4_1_newsfeed.entity.User;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@RequiredArgsConstructor
 public class FeedResponseDto {
 
     private final Long id;
+    private final String username;
     private final String contents;
     private final LocalDateTime createdAt;
-
-    public FeedResponseDto(Long id, String contents, LocalDateTime createdAt) {//, String imageUrl) {
-        this.id = id;
-        this.contents = contents;
-        this.createdAt = createdAt;
-    }
-
 }
-
-
-
