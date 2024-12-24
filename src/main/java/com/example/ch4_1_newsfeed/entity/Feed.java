ce51ff498df9ef6bf2f1beb3e1ff38664ca6b1e8
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-24 22:55:03 +0900

    🩹 : 안 쓰는 메서드, 임포트 삭제

--- src/main/java/com/example/ch4_1_newsfeed/entity/Feed.java
+++ src/main/java/com/example/ch4_1_newsfeed/entity/Feed.java
@@ -1,57 +1,36 @@
 package com.example.ch4_1_newsfeed.entity;
 
-import com.example.ch4_1_newsfeed.dto.feed.FeedRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "feed")
-@EntityListeners(AuditingEntityListener.class)
 public class Feed extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@Column(nullable = false)
-    //private String contents;
-
-    @Column(columnDefinition = "longtext")
+    @Column(columnDefinition = "longtext", nullable = false)
     private String contents;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", nullable = false)
-//    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-
-
-
-
-//    @Column(nullable = false)
-//    @OneToMany(mappedBy = "feed")
-//    private List<Photo> photo = new ArrayList<>();
-
     public Feed() {
     }
 
-
-
-    
+    public Feed(String contents, User user) {
+        this.contents = contents;
+        this.user = user;
+    }
 }
 
 
