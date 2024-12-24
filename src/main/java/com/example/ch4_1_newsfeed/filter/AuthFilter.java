b2c8c631c86ed7bde05a6e42033d3c89c44c03ae
Jiwon <may0399@naver.com>
2024-12-24 15:01:01 +0900

    ✨새로운 피드 작성 추가

--- src/main/java/com/example/ch4_1_newsfeed/filter/AuthFilter.java
+++ src/main/java/com/example/ch4_1_newsfeed/filter/AuthFilter.java
@@ -1,44 +1,44 @@
-package com.example.ch4_1_newsfeed.filter;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.PatternMatchUtils;
-
-import java.io.IOException;
-
-@Slf4j
-public class AuthFilter implements Filter {
-
-    public static final String[] WHITE_LIST = {"/", "/users/login", "/users/signup"};
-
-    @Override
-    public void doFilter(
-            ServletRequest request,
-            ServletResponse response,
-            FilterChain filterChain
-    ) throws IOException, ServletException {
-
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String requestURI = httpRequest.getRequestURI();
-
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        if (!isWhiteList(requestURI)) {
-
-            HttpSession session = httpRequest.getSession(false);
-
-            if (session != null || session.getAttribute("username") == null) {
-                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "unauthorized");
-            }
-        }
-
-        filterChain.doFilter(request, response);
-    }
-
-    public boolean isWhiteList(String requestURI) {
-        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
-    }
-}
+//package com.example.ch4_1_newsfeed.filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.util.PatternMatchUtils;
+//
+//import java.io.IOException;
+//
+//@Slf4j
+//public class AuthFilter implements Filter {
+//
+//    public static final String[] WHITE_LIST = {"/", "/users/login", "/users/signup"};
+//
+//    @Override
+//    public void doFilter(
+//            ServletRequest request,
+//            ServletResponse response,
+//            FilterChain filterChain
+//    ) throws IOException, ServletException {
+//
+//        HttpServletRequest httpRequest = (HttpServletRequest) request;
+//        String requestURI = httpRequest.getRequestURI();
+//
+//        HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//        if (!isWhiteList(requestURI)) {
+//
+//            HttpSession session = httpRequest.getSession(false);
+//
+//            if (session != null || session.getAttribute("username") == null) {
+//                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "unauthorized");
+//            }
+//        }
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    public boolean isWhiteList(String requestURI) {
+//        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+//    }
+//}
