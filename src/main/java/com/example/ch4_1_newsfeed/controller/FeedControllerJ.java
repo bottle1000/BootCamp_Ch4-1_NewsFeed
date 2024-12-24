7736cfaa44777307db5749633eef23d89a2fc1d0
Jiwon <may0399@naver.com>
2024-12-24 14:49:52 +0900

    ✨새로운 피드 작성 추가

--- src/main/java/com/example/ch4_1_newsfeed/controller/FeedControllerJ.java
+++ src/main/java/com/example/ch4_1_newsfeed/controller/FeedControllerJ.java
@@ -1,5 +1,36 @@
 package com.example.ch4_1_newsfeed.controller;
 
+import com.example.ch4_1_newsfeed.dto.feed.FeedRequestDto;
+import com.example.ch4_1_newsfeed.dto.feed.FeedResponseDto;
+import com.example.ch4_1_newsfeed.service.FeedServiceJ;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/feeds")
+@RequiredArgsConstructor
 public class FeedControllerJ {
 
+    private final FeedServiceJ feedServicej;
+
+    @PostMapping
+    public ResponseEntity<FeedResponseDto> save(@RequestBody FeedRequestDto requestDto) {
+
+        FeedResponseDto feedResponseDto =
+                feedServicej.save(
+                        requestDto.getContents(),
+                        requestDto.getName()
+                );
+
+        return new ResponseEntity<>(feedResponseDto, HttpStatus.CREATED);
+
+    }
+
 }
+
+
--- src/main/java/com/example/ch4_1_newsfeed/repository/UserRepository.java
+++ src/main/java/com/example/ch4_1_newsfeed/repository/UserRepository.java
@@ -2,8 +2,18 @@ package com.example.ch4_1_newsfeed.repository;
 
 import com.example.ch4_1_newsfeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    Optional<User> findUserByName(String username);
+
+    default User findUserByUsernameOrElseThrow(String username) {
+        return findUserByName(username).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist username = " + username));
+    }
     
 }
