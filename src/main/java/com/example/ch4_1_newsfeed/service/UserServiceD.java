089b62969797f0d3acdd7a3a3d288d7b9b516b8e
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-24 17:54:47 +0900

    🩹: 팔로우 관련 메서드 완성

--- src/main/java/com/example/ch4_1_newsfeed/service/UserServiceD.java
+++ src/main/java/com/example/ch4_1_newsfeed/service/UserServiceD.java
@@ -1,33 +1,42 @@
 package com.example.ch4_1_newsfeed.service;
 
 import com.example.ch4_1_newsfeed.dto.user.response.RelationshipResponseDto;
+import com.example.ch4_1_newsfeed.entity.Relationship;
 import com.example.ch4_1_newsfeed.entity.User;
+import com.example.ch4_1_newsfeed.repository.RelationshipRepository;
 import com.example.ch4_1_newsfeed.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserServiceD {
 
     UserRepository userRepository;
+    RelationshipRepository relationshipRepository;
 
-    public RelationshipResponseDto follow(Long id, HttpSession session) {
+    public RelationshipResponseDto follow(Long followeeId, HttpSession session) {
 
-        User following = (User) session.getAttribute("user");
-        User followed = userRepository.findById(id).orElseThrow();
+        Long userId = (Long) session.getAttribute("user");
+        User following = userRepository.findById(userId).orElseThrow();
+        User followed = userRepository.findById(followeeId).orElseThrow();
 
-        if (true) {
-            return new RelationshipResponseDto(following, followed, "you followed " + followed.getName());
+        Optional<Relationship> foundRelationship =
+                relationshipRepository.findRelationshipByFollower_IdAndFollowee_id
+                        (following.getId(), followed.getId());
+
+        if (foundRelationship.isEmpty()) {
+
+            Relationship relationship = new Relationship(following, followed);
+            relationshipRepository.save(relationship);
+            return new RelationshipResponseDto(following,followed,"you followed " + followed.getName());
+
         }
 
-        return new RelationshipResponseDto(following, followed, "you unfollowed " + following.getName());
+        relationshipRepository.delete(foundRelationship.get());
+        return new RelationshipResponseDto(following, followed, "you unfollowed " + followed.getName());
     }
-
-    private void unfollow(User following, User followed){
-
-    }
-
-    private void doFollow(User following, User followed){}
 }
