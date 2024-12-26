4da2287c9b5278b77e496208ddc76d2f51b2ba58
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-26 16:45:34 +0900

    🩹: 필드 타입 변경, 생성자 추가

--- src/main/java/com/example/ch4_1_newsfeed/dto/feed/response/FindAllFeedResponseDto.java
+++ src/main/java/com/example/ch4_1_newsfeed/dto/feed/response/FindAllFeedResponseDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -12,5 +14,14 @@ public class FindAllFeedResponseDto {
     private Long following_id;
     private String contents;
     private LocalDateTime createdAt;
-    private String photos;
+    private List<String> photos;
+
+    public FindAllFeedResponseDto(Long feedId, Long userId, String content, LocalDateTime createdAt, String photoUrls) {
+        this.feed_id = feedId;
+        this.following_id = userId;
+        this.contents = content;
+        this.createdAt = createdAt;
+        this.photos = Arrays.asList(photoUrls.split(",")); // 문자열을 List로 변환
+    }
+
 }
