29fa6c2679640ffa5b663e050fab26b07cef9334
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-26 16:25:59 +0900

    🩹: 피드 컨트롤러 메서드 병합

--- src/main/java/com/example/ch4_1_newsfeed/controller/FeedController.java
+++ src/main/java/com/example/ch4_1_newsfeed/controller/FeedController.java
@@ -1,15 +1,16 @@
 package com.example.ch4_1_newsfeed.controller;
 
-import com.example.ch4_1_newsfeed.dto.feed.FeedResponseDto;
-import com.example.ch4_1_newsfeed.dto.feed.request.ModifyFeedRequestDto;
+import com.example.ch4_1_newsfeed.dto.feed.request.FeedRequestDto;
+import com.example.ch4_1_newsfeed.dto.feed.response.FeedResponseDto;
 import com.example.ch4_1_newsfeed.dto.feed.response.FindAllFeedResponseDto;
 import com.example.ch4_1_newsfeed.dto.feed.response.FindByUserAndFeedIdResponseDto;
 import com.example.ch4_1_newsfeed.dto.feed.response.FindByUserIdResponseDto;
-import com.example.ch4_1_newsfeed.service.FeedServiceT;
+import com.example.ch4_1_newsfeed.service.FeedService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +20,25 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/feeds")
-public class FeedControllerT {
+public class FeedController {
 
-    private final FeedServiceT feedServiceT;
+    private final FeedService feedServiceT;
+
+    /**
+     * 피드 생성
+     */
+    @PostMapping
+    public ResponseEntity<FeedResponseDto> save(@RequestBody FeedRequestDto requestDto) {
+
+        FeedResponseDto feedResponseDto =
+                feedServiceT.save(
+                        requestDto.getContents(),
+                        requestDto.getName()
+                );
+
+        return new ResponseEntity<>(feedResponseDto, HttpStatus.CREATED);
+
+    }
 
     /**
      * 모든 피드조회 <br>
@@ -29,12 +46,13 @@ public class FeedControllerT {
      * - todo : page랑 size값 받아오기만 했고 구현은 추후에 할 예정
      */
     @GetMapping
-    public ResponseEntity<List<FindAllFeedResponseDto>> findAllFeeds(
+    public ResponseEntity<Page<FindAllFeedResponseDto>> findAllFeeds(
         @Valid @NotNull(message = "page가 포함되어야 합니다.") @Positive(message = "page는 양의 정수여야 합니다.") @RequestParam int page,
         @Valid @Positive(message = "size는 양의 정수여야 합니다.") @RequestParam int size
     ) {
 
-        return new ResponseEntity<>(feedServiceT.findAllFeeds(page, size), HttpStatus.OK);
+        Page<FindAllFeedResponseDto> allFeeds = feedServiceT.findAllFeeds(page,size);
+        return new ResponseEntity<>(allFeeds, HttpStatus.OK);
     }
 
     /**
@@ -66,7 +84,18 @@ public class FeedControllerT {
      */
     @PutMapping("/{feed_id}")
     public ResponseEntity<FeedResponseDto> modifyFeed(@PathVariable("feed_id") Long feed_id, @RequestBody ModifyFeedRequestDto dto) {
-        FeedResponseDto feedResponseDto = feedServiceT.updateFeed(feed_id, dto);
+        FeedResponseDto feedResponseDto = feedService.updateFeed(feed_id, dto);
         return ResponseEntity.ok(feedResponseDto);
+    }
+
+    /**
+     * 피드 삭제
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        feedServiceT.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
