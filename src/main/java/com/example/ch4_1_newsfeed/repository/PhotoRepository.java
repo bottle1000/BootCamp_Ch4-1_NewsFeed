3f0fdf5a7976d1b2360f85e84c64a7eb0ecab102
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-26 16:24:44 +0900

    ✨: JPQL 쿼리문 삽입

--- src/main/java/com/example/ch4_1_newsfeed/repository/PhotoRepository.java
+++ src/main/java/com/example/ch4_1_newsfeed/repository/PhotoRepository.java
@@ -10,4 +10,8 @@ import java.util.List;
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
     List<Photo> findPhotoByFeed_id(Long id);
+
+
+    @Query("select p from Photo p where p.feed.id in :feedIds")
+    List<Photo> findPhotosByFeedIds(List<Long> feedIds);
 }
