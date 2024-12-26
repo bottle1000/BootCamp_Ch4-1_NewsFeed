d6880e12085562c1cf791925272259289be64805
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2024-12-26 09:56:41 +0900

    🩹: 줄바꿈 수정

--- src/main/java/com/example/ch4_1_newsfeed/repository/UserRepository.java
+++ src/main/java/com/example/ch4_1_newsfeed/repository/UserRepository.java
@@ -4,13 +4,10 @@ import com.example.ch4_1_newsfeed.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     Optional<User> findUserByName(String username);
 
     default User findUserByUsernameOrElseThrow(String username) {
@@ -21,6 +18,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
      * null 예외를 처리하기 위해 Optional을 추가함
      */
     Optional<User> findByEmail(String email);
-
-    
 }
